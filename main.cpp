52abac620fe682d525a3ffb4910d63f8e795c838
Jesperijk <jesperijkontio@gmail.com>
2026-02-07 18:46:19 +0200

    viikkotehtava 5

--- main.cpp
+++ main.cpp
@@ -2,94 +2,91 @@
 #include <string>
 using namespace std;
 
-class Chef {
-    public:
-    Chef(string n);
-    ~Chef();
-    string getName();
-    int makeSalad(int s);
-    int makeSoup(int s);
-
-    protected:
-        string chefName;
-
-};
-
-class ItalianChef : public Chef {
-    public:
-    ItalianChef(string n);
-    ~ItalianChef();
-    bool askSecret(string n, int s, int m);
-
-    private:
-    string password = "pizza";
-    int flour;
-    int water;
-    int makepizza(int s, int m);
-
-};
-
-Chef::Chef(string n) {
-    chefName=n;
-    cout<<"Chef "<<chefName<<" konstruktori "<<endl;
-}
-
-Chef::~Chef() {
-    cout<<"Chef "<<chefName<<" destruktori "<<endl;
-}
-
-string Chef::getName() {
-    return chefName;
-}
-
-int Chef::makeSalad(int s) {
-    int portions=s/5;
-    cout<<"Chef "<<chefName<<" with "<<s<<" items can make salad "<<portions<<" portions "<<endl;
-    return portions;
-}
-
-int Chef::makeSoup(int s) {
-    int portions=s/3;
-    cout<<"Chef "<<chefName<<" with "<<s<<" items can make soup "<<portions<<" portions "<<endl;
-    return portions;
-}
-
-ItalianChef::ItalianChef(string n):Chef(n) {
-    flour=0;
-    water=0;
-    cout<<"ItalianChef "<<chefName<<" konstruktori "<<endl;
-}
-
-ItalianChef::~ItalianChef() {
-    cout<<"ItalianChef "<<chefName<<" destruktori "<<endl;
-}
-
-bool ItalianChef::askSecret(string n,int s,int m){
-    flour=s;
-    water=m;
-
-    if(n==password) {
-        cout<<"Password ok!"<<endl;
-        int pizzas=makepizza(flour,water);
-        cout<<"ItalianChef "<< chefName <<" with "<<flour<<" flour and "<<water<<" water can make "<<pizzas<<" pizzas "<<endl;
-        return true;
+class Seuraaja {
+public:
+    Seuraaja *next = nullptr;
+    Seuraaja(string nimi) : nimi(nimi) {
+        cout << "Luodaan seuraaja " << nimi << endl;
     }
-    cout<<"Wrong password!"<<endl;
-    return false;
+    string getNimi() {
+        return nimi;
+    }
+    void paivitys(string viesti) {
+        cout << "Seuraaja " << nimi << " sai viestin " << viesti << endl;
+    }
+private:
+    string nimi;
+};
+
+class Notifikaattori {
+private:
+    Seuraaja *seuraajat = nullptr;
+public:
+    Notifikaattori() {
+        cout << "Luodaan notifikaattori" << endl;
 }
 
-int ItalianChef::makepizza(int s,int m) {
-    return min(s/5,m/5);
+    void lisaa(Seuraaja *uusi) {
+        cout << "Notifikaattori lisaa seuraajan " << uusi->getNimi() << endl;
+        uusi->next = seuraajat;
+        seuraajat = uusi;
 }
-int main(){
-    Chef c("Gordon");
-    c.makeSalad(11);
-    c.makeSoup(14);
 
-    ItalianChef i("Mario");
-    i.makeSalad(9);
-    i.askSecret("pizza",12,12);
+
+    void poista(Seuraaja *poistettava) {
+        cout << "Notifikaattori poistaa seuraajan " << poistettava->getNimi() << endl;
+        if (seuraajat == nullptr)
+            return;
+
+        if (seuraajat == poistettava) {
+            seuraajat = seuraajat->next;
+            return;
+}
+
+        Seuraaja *nykyinen = seuraajat;
+        while (nykyinen->next != nullptr) {
+            if (nykyinen->next == poistettava) {
+                nykyinen->next = poistettava->next;
+                return;
+            }
+            nykyinen = nykyinen->next;
+}
+}
+
+    void tulosta() {
+        cout << "Notifikaattorin seuraajat:" << endl;
+        Seuraaja *nykyinen = seuraajat;
+        while (nykyinen != nullptr) {
+            cout << "Seuraaja " << nykyinen->getNimi() << endl;
+            nykyinen = nykyinen->next;
+        }
+    }
+
+    void postita(string viesti) {
+        cout << "Notifikaattori postaa viestin " << viesti << endl;
+        Seuraaja *nykyinen = seuraajat;
+        while (nykyinen != nullptr) {
+            nykyinen->paivitys(viesti);
+            nykyinen = nykyinen->next;
+        }
+    }
+
+};
+
+int main()
+{
+    Notifikaattori n;
+    Seuraaja a("A");
+    Seuraaja b("B");
+    Seuraaja c("C");
+
+    n.lisaa(&a);
+    n.lisaa(&b);
+    n.lisaa(&c);
+    n.tulosta();
+    n.postita("Tama on viesti 1");
+    n.poista(&b);
+    n.postita("Tama on viesti 2");
 
     return 0;
 }
-
